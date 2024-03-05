6c396f7856d8c96a62aa4026af63e7218fa3bbba
Younus Ali <younus_ali45@icloud.com>
2024-03-05 01:37:44 -0600

    The class of Anagrammer

---
+++ Anagrams_Project/AnagramsClass.java
@@ -0,0 +1,15 @@
+class Anagrammer
+{
+  //Make an instance of Words that reads words from a text file. Make an empty AnagramTree. Read all the words from the text file and add them to the tree. Finally, traverse the tree to print all its anagrams.
+  public static void main(String[] args)
+  {
+    Words words = new Words("warAndPeace.txt");
+    AnagramTree tree = new AnagramTree();
+    while (words.hasNext())
+    {
+      String word = words.next();
+      tree.add(word);
+    }
+    tree.anagrams();
+  }
+}
